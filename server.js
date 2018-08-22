1bf21e6190d90780f96de997b73ca64f7b13413e
NDKivi <kivineal@outlook.com>
2018-08-22 12:03:52 -0500

    added Mongoose to server.js

--- server.js
+++ server.js
@@ -3,10 +3,18 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const router = require("./client/routes");
+const mongoose = require("mongoose");
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/Project3";
+
+// Connect to Mongo DB
+mongoose.connect(MONGODB_URI);
+
 
 // Define middleware here
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(router);
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
