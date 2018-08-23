c6eb26c4730d6a87bde87e7047c860e6d2efca6c
NDKivi <kivineal@outlook.com>
2018-08-23 10:09:10 -0500

    login and registration api working

--- register.js
+++ register.js
@@ -1,44 +1,56 @@
 const db = require('./client/models');
 const crypto = require("crypto");
 const router = require("express").Router();
-const bodyParser = require("body-parser");
-router.use(bodyParser);
-
-router.post("/register", function (req, res) {
-    console.log("in /register");
-    console.log(req.body);
-    return res.json(req.body);
-    db.User.findOne({ username: req.body.username.toLowerCase() }, function (err, dbUser) {
-        if (err) {
-            console.log(err);
-        }
-        if (dbUser) {
-            res.json({ "success": false, "error": "Username already taken, pick another" });
-        } else {
-            let newUser;
-            newUser["username"] = req.body.username;
-            newUser["email"] = req.body.email;
-
-            newUser["password_salt"] = crypto.randomBytes(132).toString('hex').slice(0, 132);
-            let hash = crypto.createHmac("sha512", newUser["password_salt"]);
-            hash.update(req.body.password);
-            newUser["password_hash"] = hash.digest("hex");
-
-            db.User.create(newUser, function (errorOnCreation, createdUser) {
-                if (errorOnCreation) {
-                    console.log(errorOnCreation);
-                }
-                req.login(createdUser, function (erronOnLogin) {
-                    if (erronOnLogin) {
-                        console.log(erronOnLogin);
-                    }
-                    res.redirect("/home");
-                });
+const ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn;
 
 
-            });
-        }
+module.exports = function (passport) {
+
+    router.post('/login', passport.authenticate("local"), function (req, res) {
+        res.json(req.user);
     });
-});
 
-module.exports = router;
+    router.post("/register", function (req, res) {
+        console.log("in /register");
+
+        db.User.findOne({ username: req.body.username.toLowerCase() }, function (err, dbUser) {
+
+            if (err) {
+                console.log(err);
+            }
+            if (dbUser) {
+                res.json({ "success": false, "error": "Username already taken, pick another" });
+            } else {
+                const newUser = {};
+                newUser["username"] = req.body.username;
+                newUser["email"] = req.body.email;
+                newUser["password_salt"] = crypto.randomBytes(132).toString('hex').slice(0, 132);
+                let hash = crypto.createHmac("sha512", newUser["password_salt"]);
+                hash.update(req.body.password);
+                newUser["password_hash"] = hash.digest("hex");
+                db.User.create(newUser, function (errorOnCreation, createdUser) {
+                    if (errorOnCreation) {
+                        console.log(errorOnCreation);
+                    }
+                    req.login(createdUser, function (erronOnLogin) {
+                        if (erronOnLogin) {
+                            console.log(erronOnLogin);
+                        }
+                        res.redirect("/home");
+                    });
+                });
+            }
+        });
+    });
+
+    router.get("/logout", function(req, res) {
+        req.logout();
+        res.redirect("/");
+    });
+
+    router.get("/something", ensureLoggedIn(), function(req, res) {
+        res.json({success:(req.user? "Yes":"No"), user:req.user});//test path protection
+    });
+
+    return router;
+}
