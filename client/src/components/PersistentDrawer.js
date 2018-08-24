3f320e091a236319cc340cc8b09475b116328e16
KMonahan <kevin.monahan@hotmail.com>
2018-08-24 13:51:46 -0500

    pulling master

--- client/src/components/PersistentDrawer.js
+++ client/src/components/PersistentDrawer.js
@@ -237,7 +237,7 @@ class PersistentDrawer extends React.Component {
                             </ListItemIcon>
                             <ListItemText primary="Spam" />
                         </ListItem>
-                        
+
                         {/* </div> */}
                     </Drawer>
 
