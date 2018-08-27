a6ecbf40a4d025c660fd2c0366134f9273132818
NDKivi <kivineal@outlook.com>
2018-08-27 11:51:40 -0500

    added component for past articles

--- client/src/components/PersistentDrawer.js
+++ client/src/components/PersistentDrawer.js
@@ -141,6 +141,7 @@ class PersistentDrawer extends React.Component {
             articleText: "",
 
             currentArticleId: "",
+            articleNumber: "",
 
             regusername: "",
             logusername: "",
@@ -415,6 +416,27 @@ class PersistentDrawer extends React.Component {
         
     }
 
+    // getHeadline = (articleId) => {
+    //     fetch('/api/articles/' + articleId)
+    //     .then(response => response.json())
+    //     .then(myJson => {
+    //         console.log("trying to fetch read article headline");
+    //         this.setState({headline: myJson[0].headline});
+    //     })
+    //     .catch(err => {
+    //         console.log(err);
+    //     });
+    // }
+    
+    handlePastArticle = (articleId) => {
+        console.log("inside handleSelectPastArticle");
+        console.log(articleId);
+        let currentVersion = this.state.articleNumber;
+        currentVersion++;
+        this.setState({"currentArticleId": articleId, "articleNumber": currentVersion});
+        console.log(this.state.currentArticleId);
+    }
+    
 
 
 ////Drawer Methods/////////////////////////////////////////////////////////////////////////////////////
@@ -557,7 +579,19 @@ class PersistentDrawer extends React.Component {
 
                     
             {/* Map of past articles should go here */}
-
+            <tbody>
+                {console.log(this.state.user)}
+                {(!this.state.loggedIn)
+                ?<p>Placeholder</p>
+                :this.state.user.articles.map((article, i) => {
+                    return (<ListItem key={i} button onClick={() => { this.handlePastArticle(article); }}>
+                        <ListItemIcon>
+                            <LogoutIcon />
+                        </ListItemIcon>
+                        <ListItemText primary={i + 1} />
+                    </ListItem>);
+                })}
+            </tbody>
                     
 
                     </Drawer>
@@ -575,7 +609,7 @@ class PersistentDrawer extends React.Component {
                         
 
             {/* The article */}
-                            <Article article={currentArticleId}/>
+                            <Article article={currentArticleId} key={this.state.articleNumber}/>
 
             {/* Reactions */}
                             {this.state.reacted ? <Reactions key={this.state.forceUpdate} handleUser={this.handleCreateChat} articleId={currentArticleId} /> : ""}
